c5dbc7df221c290582348337a1c4714c4d6cc637
Maninderjit Bindra <mani@Saurabhs-MacBook-Pro.local>
2017-08-02 14:24:39 +0530

    modified gulp file to pass in FACE_API_KEY

--- services/photos/gulpfile.js
+++ services/photos/gulpfile.js
@@ -2,8 +2,13 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var util = require('gulp-util');
+var argv = require('yargs').argv;
  
 gulp.task('test', function () {
+  if(argv.FACE_API_KEY != undefined)
+    {
+        process.env.FACE_API_KEY = argv.FACE_API_KEY;
+    }
   return gulp
   .src('test/**/*.js', {read: false})
   .pipe(mocha())
