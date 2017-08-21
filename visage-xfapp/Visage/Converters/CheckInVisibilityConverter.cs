82adc80655d58ec05b152cfa48162aa6105b1428
Hardik Mistry <mistryhardik.05@gmail.com>
2017-08-21 23:08:01 +0530

    Refactor: events, session, include tabbed pages for event and session

---
+++ visage-xfapp/Visage/Converters/CheckInVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Visage.Converters
+{
+    public class CheckInVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                if(string.IsNullOrEmpty(value as string))
+                {
+                    var eventStartDate = value as string;
+
+                    var today = DateTime.Now.ToString("d");
+
+                    return eventStartDate.Equals(today);
+                }
+
+                return false;
+            }
+
+            return false;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ visage-xfapp/Visage/ViewModels/EventDetailsPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using Visage.Helpers;
+using Visage.Models;
+using Xamarin.Forms;
+
+namespace Visage.ViewModels
+{
+    public class EventDetailsPageViewModel : BaseViewModel
+    {
+        public EventDetailsPageViewModel(string title)
+        {
+            Title = title;
+        }
+    }
+}
