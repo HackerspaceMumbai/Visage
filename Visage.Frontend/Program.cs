f8cdfc46e1037083ed0a45cc3c5e926e2942c8f6
Augustine Correa <augcor@gmail.com>
2020-03-29 19:29:37 +0530

    Renamed Blazor app to Visage.Frontend

--- Visage.Frontend/Program.cs
+++ Visage.Frontend/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Frontend
+namespace Visage.Frontend
 {
     public class Program
     {
@@ -17,6 +17,8 @@ namespace Frontend
             builder.Services.AddBaseAddressHttpClient();
             builder.Services.AddOidcAuthentication(options =>
             {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
                 options.ProviderOptions.Authority = "https://indcoder.auth0.com";
                 options.ProviderOptions.ClientId = "LmyPloWc6iTqUqZ0iOsXEc2XjcGAulen";
             });
@@ -25,4 +27,3 @@ namespace Frontend
         }
     }
 }
-    
