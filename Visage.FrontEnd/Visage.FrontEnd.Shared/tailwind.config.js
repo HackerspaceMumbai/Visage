51cbe31a04a1f28734149f30b2a0e3fc0b70245f
Augustine Correa <augcor@gmail.com>
2025-05-20 16:28:16 +0530

    style: :lipstick: Upgrade Daisy UI to version 5 from 3
    1. Upgraded Tailwind 2. Edited Tailwind config to cater for the change in config  2. Upgraded daisy UI
    
    Closes #130
    
    Signed-off-by: Augustine Correa <augcor@gmail.com>

--- Visage.FrontEnd/Visage.FrontEnd.Shared/tailwind.config.js
+++ Visage.FrontEnd/Visage.FrontEnd.Shared/tailwind.config.js
@@ -4,42 +4,7 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require('daisyui')],
-    daisyui: {
-        themes: [
-
-            {
-                light: {
-                    ...require("daisyui/src/theming/themes")["light"],
-                    "primary": "facc15",
-                    "secondary": "003366",
-
-                },
-
-                dark: {
-                    ...require("daisyui/src/theming/themes")["dark"],
-                    "primary": "facc15",
-                    "primary-content": "111111",
-                    "secondary": "ffffff"
-
-                },
-
-
-
-
-
-            },
-
-
-        ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: "false", // name of one of the included themes for dark mode
-        base: true, // applies background color and foreground color for root element by default
-        styled: true, // include daisyUI colors and design decisions for all components
-        utils: true, // adds responsive and modifier utility classes
-        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-        themeRoot: ":root", // The element that receives theme color CSS variables
-    }
-
+    
 }
+
 
